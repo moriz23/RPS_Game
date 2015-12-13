fff9bf0f08ca48396652cf7e62283532cef3a3ad
Mohammed Rizvi <morizvi07@gmail.com>
2015-12-13 17:59:39 -0500

    added click function for images and shows results in console

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-  alert("hello");
+  var userChoice;
+  var computerChoice;
+  var userScore = 0;
+  var computerScore = 0;
+  var roundCounter = 1;
+  var computerOptions = [
+    "Rock",
+    "Paper",
+    "Scissors"
+  ]
+
+  $("a").on("click", function() {
+    userChoice = this.id;
+    computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+    console.log(userChoice, computerChoice);
+  });
 });
