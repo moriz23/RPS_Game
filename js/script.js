f9c094afc697c9f33b76d1cdbc2238bf357adbbc
Mohammed Rizvi <morizvi07@gmail.com>
2015-12-13 18:05:38 -0500

    Added if else statements for rps logic

--- js/script.js
+++ js/script.js
@@ -14,5 +14,44 @@ $(document).ready(function() {
     userChoice = this.id;
     computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
     console.log(userChoice, computerChoice);
+    if(userChoice === computerChoice) {
+        alert("TIE");
+      } 
+
+        else if (userChoice === "Rock" && computerChoice === "Scissors") {
+        alert("YOU WIN");
+        userScore++;
+        $(".userScore").html(userScore);
+
+      } else if (userChoice === "Rock" && computerChoice === "Paper") {
+        alert("YOU LOST");
+        computerScore++;
+        $(".computerScore").html(computerScore);
+      } 
+
+        else if (userChoice === "Paper" && computerChoice === "Scissors") {
+        alert("YOU LOST");
+        computerScore++;
+        $(".computerScore").html(computerScore);
+
+      } else if (userChoice === "Paper" && computerChoice === "Rock") {
+        alert("YOU WIN");
+        userScore++;
+        $(".userScore").html(userScore);
+
+      } 
+        else if (userChoice === "Scissors" && computerChoice === "Rock") {
+        alert("YOU LOST");
+        computerScore++;
+        $(".computerScore").html(computerScore);
+      } else if (userChoice === "Scissors" && computerChoice === "Paper") {
+        alert("YOU WIN");
+        userScore++;
+        $(".userScore").html(userScore);
+      }
+      roundCounter++;
+      $(".round").html(roundCounter);
+      console.log(userChoice, computerChoice);
+      
   });
 });
