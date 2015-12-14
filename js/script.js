916b839faf23c901e582ed79a0ce003e4a678a18
Mohammed Rizvi <morizvi07@gmail.com>
2015-12-14 14:19:49 -0500

    Added gifs to modal and created a function that tells the user who won out of five rounds

--- js/script.js
+++ js/script.js
@@ -23,52 +23,106 @@ $(document).ready(function() {
 
   $("a").hover(function () {
   $(this).addClass("pulse");
-});
-
+  });
 
   $("a").on("click", function() {
     userChoice = this.id;
     computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-    console.log(userChoice, computerChoice);
     if(userChoice === computerChoice) {
-        alert("TIE");
+        $("h4").html("TIE GAME: You chose " + userChoice + " and Computer chose " + computerChoice);
+        $("p").html("<img id='yo' src='http://www.reactiongifs.com/r/2013/02/tied.gif'>");
+        $("#myModal").modal("show");
+        gameOver();
+        roundCounter++;
+        $(".round").html(roundCounter);
       } 
 
         else if (userChoice === "Rock" && computerChoice === "Scissors") {
-        alert("YOU WIN");
+        $("h4").html("YOU WON: You chose " + userChoice + " and Computer chose " + computerChoice);
+        $("p").html("<img id='yo' src='https://usatftw.files.wordpress.com/2015/02/carltonjimmyfallon.gif?w=1000'>");
+        $("#myModal").modal("show");
+        gameOver();
         userScore++;
+        roundCounter++;
+        $(".round").html(roundCounter);
         $(".userScore").html(userScore);
 
       } else if (userChoice === "Rock" && computerChoice === "Paper") {
-        alert("YOU LOST");
+        $("h4").html("FAILED! COMPUTER WON: You chose " + userChoice + " and Computer chose " + computerChoice);
+        $("p").html("<img id='yo' src='http://i748.photobucket.com/albums/xx124/ohkimosabe/GIFS/2ykx2msjpg.gif'>");
+        $("#myModal").modal("show");
+        gameOver();
         computerScore++;
+        roundCounter++;
+        $(".round").html(roundCounter);
         $(".computerScore").html(computerScore);
       } 
 
         else if (userChoice === "Paper" && computerChoice === "Scissors") {
-        alert("YOU LOST");
+        $("h4").html("FAILED! COMPUTER WON: You chose " + userChoice + " and Computer chose " + computerChoice);
+        $("p").html("<img id='yo' src='http://thirdmonk.net/postcont/2013/10/Trampoline-Dunk-Fail.gif'>");
+        $("#myModal").modal("show");
+        gameOver();
         computerScore++;
+        roundCounter++;
+        $(".round").html(roundCounter);
         $(".computerScore").html(computerScore);
 
       } else if (userChoice === "Paper" && computerChoice === "Rock") {
-        alert("YOU WIN");
+        $("h4").html("YOU WON: You chose " + userChoice + " and Computer chose " + computerChoice);
+        $("p").html("<img id='yo' src='http://gifrific.com/wp-content/uploads/2012/08/LeBron-James-Dancing-After-Winning-Gold-Medal-2012-London-Olympics.gif'>");
+        $("#myModal").modal("show");
+        gameOver();
         userScore++;
+        roundCounter++;
+        $(".round").html(roundCounter);
         $(".userScore").html(userScore);
 
       } 
         else if (userChoice === "Scissors" && computerChoice === "Rock") {
-        alert("YOU LOST");
+        $("h4").html("FAILED! COMPUTER WON: You chose " + userChoice + " and Computer chose " + computerChoice);
+        $("p").html("<img id='yo' src='http://45.media.tumblr.com/tumblr_ltwddhOT1T1qfetl0o1_500.gif'>");
+        $("#myModal").modal("show");
+        gameOver();
         computerScore++;
+        roundCounter++;
+        $(".round").html(roundCounter);
         $(".computerScore").html(computerScore);
 
       } else if (userChoice === "Scissors" && computerChoice === "Paper") {
-        alert("YOU WIN");
+        $("h4").html("YOU WON: You chose " + userChoice + " and Computer chose " + computerChoice);
+        $("p").html("<img id='yo' src='http://www.tapfortap.com/wp-content/uploads/2015/06/tumblr_ln9eat2mn61qa9fb5o1_500.gif'>");
+        $("#myModal").modal("show");
+        gameOver();
         userScore++;
+        roundCounter++;
+        $(".round").html(roundCounter);
         $(".userScore").html(userScore);
       }
-      roundCounter++;
-      $(".round").html(roundCounter);
+
+    
+      
       console.log(userChoice, computerChoice);
       
   });
+  function gameOver() {
+        if (roundCounter === 6 && userScore > computerScore) {
+        $("h4").html("CONGRATS YOU WON THE MOST ROUNDS");
+        $("p").html("<img id='yo' src='https://media.giphy.com/media/iXmF48FlrGuTS/giphy.gif'>");
+        $("#myModal").modal("show");
+
+      } else if (roundCounter === 6 && computerScore > userScore) {
+        $("h4").html("SERIOUSLY? THE COMPUTER WON MORE ROUNDS THAN YOU?");
+        $("p").html("<img id='yo' src='http://i.imgur.com/1HWQIPa.gif'>");
+        $("#myModal").modal("show");
+      
+      } else if (roundCounter === 6 && computerScore === userScore) {
+        $("h4").html("YOU CAN KEEP SAYING THAT TO YOURSELF, BUT IT IS A TIE");
+        $("p").html("<img id='yo' src='https://jessrunshappy.files.wordpress.com/2014/05/be-positive.gif'>");
+        $("#myModal").modal("show");
+      }
+
+      } 
+
+  
 });
